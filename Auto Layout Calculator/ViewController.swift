06aa401aed261bb0dbc8df434d6883a54047e9ae
Angel Solorio <angel.solorio@me.com>
2019-02-06 17:47:08 -0600

    Initial Commit

---
+++ Auto Layout Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Auto Layout Calculator
+//
+//  Created by Angel Solorio on 2/6/19.
+//  Copyright © 2019 Angel Solorio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
